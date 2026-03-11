b7a60da5f3f227f478ea6d2e230ec6d399cbc16c
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-03-12 01:44:28 +0600

    Module-45

--- src/app/(commonLayout)/(authRouteGroup)/login/page.tsx
+++ src/app/(commonLayout)/(authRouteGroup)/login/page.tsx
@@ -1,5 +1,11 @@
+import LoginForm from "@/components/modules/Auth/LoginForm";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
